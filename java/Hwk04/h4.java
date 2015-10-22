7b7f7d1a54f630adf4a462532e7f2ae8256767c6
Chris Fenton <cfenton30@gmail.com>
2015-10-21 22:02:14 -0700

    java/hw4

--- java/Hwk04/h4.java
+++ java/Hwk04/h4.java
@@ -1,7 +1,7 @@
 /*
  *  File:           h4a.java
  *  Author:         Chris Fenton
- *  Last modified:  10/20/2015
+ *  Last modified:  10/21/2015
  * 
  *  This class includes methods that use the for statement
  */
@@ -101,6 +101,17 @@ public class h4 {
   /* sumPropDivisors() takes a number, x, and
    * returns the sum of its proper divisors
    */
+  public static int sumPropDivisors(int x) {
+    int result = 0;
+    
+    for(int i=1;i<x;i++) {
+      if(x % i == 0) {
+        result += i;
+      }
+    }
+    
+    return result;
+  }
  
   
 //---------------------------------
@@ -110,17 +121,100 @@ public class h4 {
    * is perfect, a nummber that equals the sum
    * of its proper divisors
    */
+  public static boolean isPerfect(int x) {
+    boolean result = false;
+    
+    if(sumPropDivisors(x) == x) {
+      result = true;
+    }
+    
+    return result;
+  }
   
 //---------------------------------
   
   /* isPrime() takes an integer x and determines
    * if the number is prime, returning a boolean.
    */
+  public static boolean isPrime(int x) {
+    boolean result = false;
+    int sumProp = sumPropDivisors(x);
+    
+    if(sumProp == 1 && x != 1) {
+      result = true;
+    }
+    
+    return result;
+  }
 
 
 //---------------------------------
+  
+  //LOOK at THE PDF FOR THE TWO "EXTRA CREDIT" problems!
+  
+  /* nthPerfect() takes an integer x and determines
+   * the nth perfect number, returning an integer.
+   */  
+  public static int nthPerfect(int x) {
+    int result = 0;
+    int i = 1; 
+    int k = 1;
+    
+    for(int n=1;k<=x;n++) {   
+      if(isPerfect(i)) {
+        result = i;
+        k += 1;
+      }
+      i += 1;              
+    }
+    
+    return result;
+  }
+  
+//---------------------------------
+  
+  /* isAmicable() takes two integers,  x and y, and determines
+   * if they are amicable, returning an boolean.
+   */
+  public static boolean isAmicable(int x, int y) {
+    boolean result = false;
+    
+    if(x != y) {
+      if(sumPropDivisors(x) == y && sumPropDivisors(y) == x) {
+        result = true;
+      }
+    }
+    
+    return result;
+  }
+  
+//---------------------------------
+  /* nthAmicablePair() takes an integer x (less than 8) and determines
+   * the nth amicable pair, printing the first x amicable pairs to the console.
+   * restricting x to integers below 8 just to prevent long execution times.
+   * inspiration from https://www.youtube.com/watch?v=IbhBki25KGc and projectEuler.
+   */
+  public static void nthAmicablePair(int x) {
+    assert x < 8 : "x is greater than 8";
+    
+    int start = 2;
+    int i = 1;
+       
+    for(int n=start;i<=x;n++) {
+      int newNum = sumPropDivisors(n);
+      if (newNum <= n) { // jump ahead if newNum is less than n
+        continue;
+      }
+      if(isAmicable(newNum,n)) {
+        System.out.println(i + " " + n + "," + newNum);
+        i += 1;
+      }  
+    }       
+    
+  }
+//---------------------------------
+  
 
-//LOOK at THE PDF FOR THE TWO "EXTRA CREDIT" problems!
   
 //==========================================
   
