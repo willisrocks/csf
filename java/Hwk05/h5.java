619e95ff04191064e02a24b3c8871093d1ea45a8
Chris Fenton <cfenton30@gmail.com>
2015-10-27 10:40:26 -0700

    added java/hwk5

---
+++ java/Hwk05/h5.java
@@ -0,0 +1,21 @@
+/*
+ *  File:           h5.java
+ *  Author:         <name>
+ *  Last modified:  <date>
+ * 
+ *  This class contains mathemtical functions built
+ *  entirely from the increment and decrement
+ *  operators.
+ */
+
+//==================================================
+
+public class h5 {
+  
+//==================================================
+  
+ // ALL OF YOUR CODE GOES HERE!
+  
+//==================================================
+  
+} // end class h5
