53b6b7c4f819f6b07e8ab20fd47f11e7c5772d87
Chris Fenton <cfenton30@gmail.com>
2016-04-22 19:02:54 -0700

    fixed indentation

--- sos/lab2/ourDfs.cpp
+++ sos/lab2/ourDfs.cpp
@@ -1,34 +1,34 @@
 /*
- * This version just reads the current directory
- * information and outputs the name and if it is a file or a directory.
- */
-       #include <stdio.h>
-       #include <string.h>
-	   #include <sys/types.h>
-       #include <dirent.h>
-       #include <sys/types.h>
-       #include <sys/stat.h>
-       #include <unistd.h>
-	   #include <iostream>
-	   #include <string>
-	   #include "arrayStack.h"
-	   #include "arrayQueue.h"
-	   using namespace std;
+* This version just reads the current directory
+* information and outputs the name and if it is a file or a directory.
+*/
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <iostream>
+#include <string>
+#include "arrayStack.h"
+#include "arrayQueue.h"
+using namespace std;
 
-void printStack(arrayStack<string> & s) {
-	cout << "Stack begin" << endl;
-	for (int i = s.stackTop; i >= 0 ; i--) {
-		cout << s.stack[i] << endl;
-	}
-	cout << "Stack end" << endl;
-}
+//void printStack(arrayStack<string> & s) {
+//	cout << "Stack begin" << endl;
+//	for (int i = s.stackTop; i >= 0 ; i--) {
+//		cout << s.stack[i] << endl;
+//	}
+//	cout << "Stack end" << endl;
+//}
 
 void dfs(arrayStack<string> & s) {
 	struct stat stat_b;
 	struct dirent *d;
 	DIR *dir;
 	string str;
-	string dirname;
+	//string dirname;
 
 	// s is the stack of directories
 
@@ -36,7 +36,6 @@ void dfs(arrayStack<string> & s) {
 		// get the top of the stack. it is a string pathname
 		// of the current directory or file you are working on
 		str = s.top();
-		cout << "STR is now " << str << endl;
 		// do the stat on that file
 		if (stat((char*)str.data(), &stat_b) != 0) {
 		}
@@ -48,29 +47,35 @@ void dfs(arrayStack<string> & s) {
 		if (S_ISREG(stat_b.st_mode)==1) {
 			cout << "The filename is:" << str << endl;
 			cout << "The file size is:" << stat_b.st_size << "bytes" << endl;
+			cout << endl;
 		} else if (S_ISDIR(stat_b.st_mode)==1) {
-		// Otherwise it is a directory
-			dir = opendir((char*)str.data());
-			cout << "The directory is:" << str << endl;
-			cout << "The directory is open" << endl;
-		//	while ( you are reading the directory) {
-			while (d = readdir(dir)) {
-				cout << d->d_name << endl;				
-				// construct the path name	
-				dirname = str + "/" + d->d_name;	
-				cout << "dirname constructed" << endl;
-				// check for . and ..
-				if (strcmp(d->d_name, ".") != 0 && strcmp(d->d_name, "..") != 0) {
-				// if not . or .. push on the stack	
-				    cout << "dirname not . or .." << endl;
-					s.push((char *)dirname.data());
-					printStack(s);
-					cout << "dirname pushed" << endl;
-				} 
+			// Otherwise it is a directory
+			cout << "Opening " << str << endl;
+
+			dir = opendir( (char*)str.data());
+			if (dir != NULL) {
+
+				//	while ( you are reading the directory) {
+				while ((d = readdir(dir)) != NULL) {
+					string dirname;
+					int dType = d->d_type;
+
+					// construct the path name
+					if (dType == 4) {
+						dirname = str + d->d_name + "/";
+					} else if (dType == 8) {
+						dirname = str + d->d_name;
+					}
+					// check for . and ..
+					if (strcmp(d->d_name, ".") != 0 && strcmp(d->d_name, "..") != 0) {
+						// if not . or .. push on the stack
+						s.push((char *)dirname.data());
+						cout << dirname << " pushed" << endl;
+					}
+				}
+				closedir(dir);
+				cout << "The directory is closed" << endl;
 			}
-			cout << "dir read" << endl;
-			closedir(dir);
-			cout << "The directory is closed" << endl;
 		}
 	}
 	return;
@@ -93,7 +98,7 @@ int main(int argc, char **argv) {
 
 	s.push(*new string(buf));
 
-	cout << "DFS" << endl;
+	cout << "DFS Started:" << endl;
 	dfs(s);
 	return 0;
 }
